f7211895c50a7b70d55cea212e9db463d3d3e62b
Matthew MacEachern <mcmaceac@red.eecs.yorku.ca>
2017-03-06 14:22:08 -0500

    Fixed to add club description, still not sure about AARP

--- seven.sql
+++ seven.sql
@@ -1,1 +1,2 @@
-select distinct club from yrb_offer O where price >= all(select O2.price from yrb_offer O2 where O2.club != O.club)
+select distinct C.club, C.desc from yrb_offer O, yrb_club C where C.club = O.club and price >= all(select O2.price from yrb_offer O2 where O2.club != O.club)
+
