71dee21f77e1f9ed860ec2ee874d0dec5cf3cc2a
Matthew MacEachern <mcm.maceachern@gmail.com>
2017-03-07 12:41:24 -0500

    Completed two and verified correctness

--- two.sql
+++ two.sql
@@ -1,1 +1,7 @@
-select C.name, max(P.qnty) from yrb_customer C, yrb_purchase P where C.cid = P.cid group by C.name
+select N.name, max(N.quant) \
+from \
+	(select name, P.club, sum(P.qnty) as quant \
+	from yrb_customer C, yrb_purchase P \
+	where C.cid = P.cid \
+	group by name, P.club) as N \
+group by N.name
