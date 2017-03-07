e849ba7ff214611d3842c8e4702c229f5b69385b
Matthew MacEachern <mcm.maceachern@gmail.com>
2017-03-07 10:55:20 -0500

    Corrected seven to be purchased instead of offered

--- seven.sql
+++ seven.sql
@@ -1,2 +1,2 @@
-select distinct C.club, C.desc from yrb_offer O, yrb_club C where C.club = O.club and price >= all(select O2.price from yrb_offer O2 where O2.club != O.club)
+select distinct C.club, C.desc from yrb_offer O, yrb_club C, yrb_purchase P where C.club = O.club and O.club = P.club and O.title = P.title and O.year = P.year and O.price >= all(select O2.price from yrb_offer O2)
 
