a22ebac6d47b8b1f9c47248aa0fdf044ad02d2bb
Matthew MacEachern <mcm.maceachern@gmail.com>
2017-03-07 11:34:55 -0500

    Delete seven.sql

--- seven.sql
+++
@@ -1,2 +0,0 @@
-select distinct C.club, C.desc from yrb_offer O, yrb_club C, yrb_purchase P where C.club = O.club and O.club = P.club and O.title = P.title and O.year = P.year and O.price >= all(select O2.price from yrb_offer O2)
-
