93e73b36832ab31c14e08ca8ba07a5e6ec781c7c
mcmaceac <mcm.maceachern@gmail.com>
2017-03-16 16:58:49 -0400

    Formatted the average difference as decimal (7, 2) and added output to output folder

--- nine.sql
+++ nine.sql
@@ -3,7 +3,7 @@ with PurchaseDates as ( \
 		ROW_NUMBER() over (order by when desc) as RowNumber, cid, when \
 	from yrb_purchase \
 ) \
-select N.cid, avg(N.diff) \
+select N.cid, cast(avg(N.diff) as decimal(7,2)) \
 from \
 	(select O1.cid, timestampdiff(16, O1.when - O2.when) as diff \
 	from PurchaseDates O1, PurchaseDates O2 \
