027bdc8f74603acab465ac87c4d3f08802ec8d23
mcmaceac <mcm.maceachern@gmail.com>
2017-03-16 21:36:22 -0400

    Completed and verified nine, included output file

--- nine.sql
+++ nine.sql
@@ -1,11 +1,11 @@
 with PurchaseDates as ( \
 	select \
-		ROW_NUMBER() over (order by when desc) as RowNumber, cid, when \
+		ROW_NUMBER() over (order by cid, when desc) as RowNumber, cid, when \
 	from yrb_purchase \
 ) \
 select N.cid, cast(avg(N.diff) as decimal(7,2)) \
 from \
-	(select O1.cid, timestampdiff(16, O1.when - O2.when) as diff \
+	(select O1.cid, cast((days(O1.when) - days(O2.when)) as decimal(7,2)) as diff \
 	from PurchaseDates O1, PurchaseDates O2 \
 	where O1.cid = O2.cid and O2.RowNumber = O1.RowNumber + 1) as N \
 group by N.cid
